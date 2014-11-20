9010246245b3e7d7091619c31caba801c8faa405
MongMX <mongmx@gmail.com>
2014-11-20 13:45:50 +0700

    Add source code

---
+++ app/src/main/java/com/mongmx/testbarcode2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mongmx.testbarcode2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ScanFragment scanFragment = new ScanFragment();
+        fragmentTransaction.add(R.id.fragment_container, scanFragment);
+//        fragmentTransaction.replace(R.id.fragment_container, scanFragment);
+//        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/mongmx/testbarcode2/ScanFragment.java
@@ -0,0 +1,78 @@
+package com.mongmx.testbarcode2;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScanFragment extends Fragment {
+
+    private String toast;
+    private EditText etSerialNo;
+
+    public ScanFragment() {}
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        displayToast();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        etSerialNo = (EditText) view.findViewById(R.id.et_serial_no);
+        Button scan = (Button) view.findViewById(R.id.scan_from_fragment);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanFromFragment();
+            }
+        });
+        return view;
+    }
+
+    public void scanFromFragment() {
+        IntentIntegrator.forSupportFragment(this)
+                .setPrompt(getResources().getString(R.string.scan_prompt))
+                .initiateScan();
+    }
+
+    private void displayToast() {
+        if(getActivity() != null && toast != null) {
+            Toast.makeText(getActivity(), toast, Toast.LENGTH_SHORT).show();
+            toast = null;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                toast = "Cancelled";
+            } else if (result.getContents().equals("5709031465291")){
+                etSerialNo.setText(result.getContents());
+                toast = "Customer found";
+            } else {
+                toast = "Customer not found";
+            }
+            displayToast();
+        }
+    }
+
+}
